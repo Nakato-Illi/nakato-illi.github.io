c1ad76f53066b64198b069a655d032f2048267ed
Nakato-Illi <illi-nakato@hotmail.de>
2024-01-27 23:41:57 +0100

    phaser game test2

--- rmaGame/game.js
+++ rmaGame/game.js
@@ -1,15 +1,11 @@
-// Our game scene
-var scene = new Phaser.Scene("game");
+window.onload = function () {
 
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    backgroundColor: 0x234232,
-    scene : scene
-};
+    var config = {
+        width: 800,
+        height: 600,
+        backgroundColor: 0x345345,
+        scene: [Scene1, Scene2]
+    }
 
-// Create the game with our config values
-// this will also inject our canvas element into the HTML source
-// for us
-var game = new Phaser.Game(config);
+    var game = new Phaser.Game(config);
+}
